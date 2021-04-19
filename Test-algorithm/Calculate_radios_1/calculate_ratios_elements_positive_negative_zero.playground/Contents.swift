21d74d91a0c7d35eb17075e0ad18ec44299afed5
Mon-STL <63224918+Mon-tri-jitti@users.noreply.github.com>
2021-04-19 18:27:11 +0700

    [upload] sample

---
+++ Test-algorithm/Calculate_radios_1/calculate_ratios_elements_positive_negative_zero.playground/Contents.swift
@@ -0,0 +1,12 @@
+func plusMinus(arr: [Int]) -> [Any]? {
+    //...
+    //... do something
+    //...
+    return nil
+}
+
+let input1 = [-1, -1, 0 , 1, 1]
+_ = plusMinus(arr: input1)
+
+let input2 = [-4, 3, -9, 0, 4, 1]
+_ = plusMinus(arr: input2)
