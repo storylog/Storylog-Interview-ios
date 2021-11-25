73652230b93c7b82a7135129c10c11a4eb9541d3
Mon Jit <mon@storylog.co>
2021-11-25 11:54:57 +0700

    [Edit] - description

--- AlgorithmTests/OverlappingSum/Overlapping_sum_array.playground/Contents.swift
+++ AlgorithmTests/OverlappingSum/Overlapping_sum_array.playground/Contents.swift
@@ -1,4 +1,4 @@
-func findSum(input1: [Int], input2: [Int]) -> Any? {
+func findSum(input1: [Int], input2: [Int], n: Int) -> Any? {
     //...
     //... do something
     //...
@@ -6,21 +6,25 @@ func findSum(input1: [Int], input2: [Int]) -> Any? {
 }
 
 /* Input A */
-let inputAOne = [1, 5, 3, 8]
-let inputATwo = [5, 4, 6, 7]
-_ = findSum(input1: inputAOne, input2: inputATwo)
+let inputA1 = [1, 5, 3, 8]
+let inputA2 = [5, 4, 6, 7]
+_ = findSum(input1: inputA1, input2: inputA2, n: 4)
+// expected == 10
 
 /* Input B*/
-let inputBOne = [1, 5, 3, 8]
-let inputBTwo = [5, 1, 8, 3]
-_ = findSum(input1: inputBOne, input2: inputBTwo)
+let inputB1 = [1, 5, 3, 8]
+let inputB2 = [5, 1, 8, 3]
+_ = findSum(input1: inputB1, input2: inputB2, n: 4)
+// expected == 34
 
 /* Input C*/
-let inputCOne = [1, 5, 5, 3, 4]
-let inputCTwo = [5, 8, 8, 7, 9]
-_ = findSum(input1: inputCOne, input2: inputCTwo)
+let inputC1 = [1, 2, 3, 4]
+let inputC2 = [5, 8, 8, 7]
+_ = findSum(input1: inputC1, input2: inputC2, n: 4)
+// expected == 0
 
-/* Input D*/
-let inputDOne = [1, 2, 3, 4, 1]
-let inputDTwo = [5, 8, 8, 7, 9]
-_ = findSum(input1: inputDOne, input2: inputDTwo)
+/* Input D */
+let inputD1 = [1, 5, 5, 5]
+let inputD2 = [5, 4, 5, 7]
+_ = findSum(input1: inputD1, input2: inputD2, n: 4)
+// expected == 20
